f6a0bc911031ab63bb7ec4bb713b11999f1969b7
permaxis <contact@permaxis.fr>
2019-09-06 10:51:12 +0200

    Add form manager

---
+++ src/app/Services/Forms/FormManager.php
@@ -0,0 +1,51 @@
+<?php
+namespace Permaxis\Core\App\Services\Forms;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\MessageBag;
+
+/**
+ * Created by PhpStorm.
+ * User: dakin
+ * Date: 04/01/2019
+ * Time: 10:50
+ */
+trait FormManager
+{
+    public function handleRequest(Request &$request)
+    {
+
+        $input = $request->all();
+
+        array_walk_recursive($input, function(&$value) {
+
+            if ($value !== '0' && empty($value))
+            {
+                $value = null;
+            }
+            else
+            {
+                $value = trim($value);
+            }
+        });
+
+        $request->replace($input);
+
+        return;
+    }
+
+    public function handleForm(Request &$request, &$entity)
+    {
+
+        $this->handleRequest($request);
+
+        $input = $request->all();
+
+        $entity = $entity->fill($input);
+
+        return;
+
+    }
+
+}
